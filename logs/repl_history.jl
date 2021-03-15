02cd64ddf1815d66f6c0577beac95fb8af01a917
William Booth <boothy.w@gmail.com>
2021-03-15 17:05:42 +0000

    added folder

--- logs/repl_history.jl
+++ logs/repl_history.jl
@@ -4,3 +4,9 @@
 # time: 2021-03-15 15:50:17 GMT Standard Time
 # mode: julia
 	import Pkg; Pkg.add("CSV")
+# time: 2021-03-15 15:57:44 GMT Standard Time
+# mode: julia
+	using CSV
+# time: 2021-03-15 16:00:33 GMT Standard Time
+# mode: julia
+	import Pkg; Pkg.add("JuMP")
